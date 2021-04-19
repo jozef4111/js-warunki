2518ae4d5aa1ba4f266ea27386c1a97402663a5e
jozef4111 <jozeff4.522@gmail.com>
2021-04-19 19:01:29 +0200

    usunąłem else if

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ let number = 12;
 
 if (number > 10) {
     console.log('Zmienna number jest wieksza niż 10')
-} else if (number === 12){
-    console.log('Spełniony warunek: number == 12')
 } else {
     console.log('Podana liczba nie jest większa od 10')
 }
